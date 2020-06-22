c55814b3c5c440c2a0b047d5e1e1c1dc48381f96
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2020-06-21 21:30:20 -0600

    Update Caso.js

--- models/Caso.js
+++ models/Caso.js
@@ -2,7 +2,7 @@ import mongoose, {Schema} from 'mongoose';
 const casoSchema = new Schema({
     nombre: { type:String,maxlength:200, required:true},
     departamento: { type:String,maxlength:50, required:true},
-    edad: { type:String,maxlength:50, required:true},
+    edad: { type:Number, required:true},
     forma_contagio: { type:String, maxlength:50, required:true},
     estado: { type:String,maxlength:50, required:true},
 }, {collection: 'casos'});

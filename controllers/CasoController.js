6f286df566e27a33f73bb3fb36079d3880f9b4ad
Oscar Rene Cuellar Mancilla <usackoka@gmail.com>
2020-07-03 16:31:42 -0600

    ultimos endpoints

--- controllers/CasoController.js
+++ controllers/CasoController.js
@@ -6,6 +6,35 @@ export default {
     list: async (req,res,next) =>{
         res.status(200).send(await models.Caso.find());
     },
+    casosCount: async (req, res, next)=>{
+        let options = [
+            { $match: { departamento: 'Guatemala', status: 'Activo'  } }, 
+            { $group: { _id: '$_id', count: {$sum: 1} } }
+        ]
+        res.status(200).send({data:await models.Caso.aggregate(options).exec()})
+    },
+    casosPie: async(req,res,next) =>{
+        const data = await models.Caso.find();
+        //map clave-departamento valor-conteo
+        const mapSort = new Map()
+        for (const departamento of data){
+            if(mapSort.has(departamento.departamento)){
+                mapSort.set(departamento.departamento,mapSort.get(departamento.departamento)+1)
+            }else{
+                mapSort.set(departamento.departamento,1)
+            }
+        }
+        //ordeno el map
+        mapSort[Symbol.iterator] = function* () {
+            yield* [...this.entries()].sort((a, b) => b[1] - a[1]);
+        }
+        //lista para la gráfica de pie x-y
+        let temp = []
+        mapSort.forEach((value,key)=>{
+            temp.push({x:key,y:value})
+        })
+        res.status(200).send(temp)
+    },
     topCasos:async(req,res,next) =>{
         const data = await models.Caso.find();
         //map clave-departamento valor-conteo
--- routes/Caso.js
+++ routes/Caso.js
@@ -3,7 +3,9 @@ import CasoController from '../controllers/CasoController';
 const router=routerx();
 
 router.get('/list', CasoController.list);
+router.get('/getPie', CasoController.casosPie)
 router.get('/listRedis', CasoController.listRedis)
 router.get('/topCasos', CasoController.topCasos)
+router.get('/casosCount', CasoController.casosCount)
 
 export default router;
